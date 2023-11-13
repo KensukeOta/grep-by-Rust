1c145ee100389ff018d71a59657d2b7c2d6c0cbd
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-11-13 16:13:52 +0900

    (clean) main関数から設定のセットアップやエラー処理に関わらない全てのロジックをrun関数に抽出しリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::env;
 use std::fs::File;
 use std::io::prelude::*;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -17,16 +18,24 @@ fn main() {
     //  {}というファイルの中
     println!("In file {}", config.filename);
 
+    if let Err(e) = run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    }
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     //  ファイルが見つかりませんでした
-    let mut f = File::open(config.filename).expect("file not founded");
+    let mut f = File::open(config.filename)?;
 
     let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        //  ファイルの読み込み中に問題がありました
-        .expect("something went wrong reading the file");
+    f.read_to_string(&mut contents)?;
 
     //  テキストは\n{}です
     println!("With text:\n{}", contents);
+
+    Ok(())
 }
 
 struct Config {
