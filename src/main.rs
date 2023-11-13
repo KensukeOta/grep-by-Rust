384b16177b34001217a3fa4780496e9ca2fe17e5
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-11-13 15:42:37 +0900

    (clean) 引数解析のエラー処理をリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        //  引数解析時に問題
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
     
     //  {}を探しています
     println!("Searching for {}", config.query);
@@ -30,10 +35,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            //  引数の数が足りません
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
