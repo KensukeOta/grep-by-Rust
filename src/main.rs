bad57061465883a5e0ee2528096f0f0875aac1ed
KensukeOta <bump_kensuke_miwa@i.softbank.jp>
2023-11-13 16:26:28 +0900

    (clean) main関数以外のコード全部をlib.rsに移動させ、移動させたコードをmain.rsのバイナリクレートのスコープに持っていくようリファクタリング

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 use std::env;
-use std::fs::File;
-use std::io::prelude::*;
 use std::process;
-use std::error::Error;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -18,40 +17,9 @@ fn main() {
     //  {}というファイルの中
     println!("In file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
 
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    //  ファイルが見つかりませんでした
-    let mut f = File::open(config.filename)?;
-
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)?;
-
-    //  テキストは\n{}です
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            //  引数の数が足りません
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
     }
 }
